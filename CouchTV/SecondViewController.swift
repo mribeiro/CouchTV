02aca100993512f7325b76c56a6a61fb114c00c1
Antonio Ribeiro <miguelribeiro.ap@gmail.com>
2016-04-12 15:08:55 +0100

    Initial Commit

---
+++ CouchTV/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CouchTV
+//
+//  Created by Antonio Ribeiro on 12/04/16.
+//  Copyright © 2016 Antonio Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
